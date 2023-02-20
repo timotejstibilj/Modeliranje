bd3151143364c560576d0c43c2487c8c7e126d72
Timotej Stibilj <timotej.stibilj@gmail.com>
2023-02-20 13:20:55 +0100

    KOMENTAR

--- vaje_1/nal_3.m
+++ vaje_1/nal_3.m
@@ -1,4 +1,8 @@
 function A = nal_3(n)
+    % A = nal_3(n) vrne matriko velikosti n*n z matriko T na diagonali in
+    % identiteto na nad/poddiagonali,
+    % funkcija kron(eye(n), X) vrne bločno diagonalno matriko z n
+    % diagonalnimi bloki iz matrike X
     T = - 4 * eye(n);
     P = diag(ones(n-1, 1), 1);
     Q = diag(ones(n -1, 1), -1);
